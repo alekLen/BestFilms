4cb4c72e95b24d8090fa4366d8307a7575c161b4
Elena <aleklenoch@gmail.com>
2023-08-20 15:48:57 +0300

    # в HomeController  добавлен метод добавления нового фильма
    состоит из двух методв: один обрабатывает Get запрос и выводит форму для добавления
    другой обрабатывает отправку формы Post запрос и производит уже добавление фильма в базу данных

--- BestFilms/Controllers/HomeController.cs
+++ BestFilms/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
 ﻿using BestFilms.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.IO;
 
 namespace BestFilms.Controllers
 {
     public class HomeController : Controller
     {
-  
+        IWebHostEnvironment _appEnvironment;
         FilmsContext db;
-        public HomeController(FilmsContext context)
+        public HomeController(FilmsContext context, IWebHostEnvironment appEnvironment)
         {          
-                db = context;           
+                db = context;
+            _appEnvironment = appEnvironment;
         }
 
         public async Task<IActionResult> Index()
@@ -19,7 +22,44 @@ namespace BestFilms.Controllers
             ViewBag.Films = f;
             return View();
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-    
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Genre,Director,Year,Story")] Film film, IFormFile photo)
+        {
+            if (photo != null)
+            {
+                // Путь к папке Files
+                string path = "/Posters/" + photo.FileName; // имя файла
+
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await photo.CopyToAsync(fileStream); // копируем файл в поток
+                }
+                Film f = new();
+                f.Name = film.Name;
+                f.Genre = film.Genre;
+                f.Director = film.Director;
+                f.Year = film.Year;
+                f.Story = film.Story;
+                f.Photo = path;
+                try
+                {
+                    db.Add(f);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    return View(film);
+                }
+            }
+            return View(film);
+        }
+
     }
 }
